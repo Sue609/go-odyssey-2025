268b6d531fad3014ee69aa42764b36f193aa7803
Sue609 <122805048+Sue609@users.noreply.github.com>
2025-09-04 10:14:06 +0300

    Practicing numbers, strings and booleans

---
+++ go-journey/basics/variables.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	var a int = 6 // explicit type
+	b := "hello, go" // type inferred
+	const pi = 3.14 // constant
+
+	fmt.Println("a =", a)
+	fmt.Println("b =", b)
+	fmt.Println("c =", pi)
+}
