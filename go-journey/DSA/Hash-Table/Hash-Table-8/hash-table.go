9b99f001e9731d68bb0ef1818d0b42882c0570e7
Sue609 <122805048+Sue609@users.noreply.github.com>
2025-10-08 10:38:25 +0300

    Adding the delete logic in our open addressing hash table implementation

--- go-journey/DSA/Hash-Table/Hash-Table-8/hash-table.go
+++ go-journey/DSA/Hash-Table/Hash-Table-8/hash-table.go
@@ -7,14 +7,16 @@ import "fmt"
 		sum = sum of ASCII of number_plate
 		return sum % length
 
-	insert(number_plate, parking_lot):
+
+	define insert(number_plate, parking_lot):
 		index = hash(number_plate, length of parking_lot)
 		start = index
-		for each number_plate[index] is occupied:
+		for each number_plate[index] is occupied and parking_lot[index] != DELETED:
 			move to the next slot index = (index + one) % length of parking_lot
 			if start == index:
 				parking lot is full
 		place index in the parking lot
+
 
 	define lookup(number_plate, paring_lot):
 		index = hash(number_plate, length of parking_lot)
@@ -24,6 +26,23 @@ import "fmt"
 				return true
 			move to next slot index = (index + one) % length of parking_lot
 		return false
+
+
+	define delete(number_plate, parking_lot):
+		index = hash(number plate, length of parking_lot)
+		start = index // to detect if we have checked all slots
+
+		while parking_lot[index] is not empty:
+			if parking_lot[index] is equal to number_plate:
+				mark slot as "DELETED"
+				print "Car removed successfully"
+				return
+			move to next slot -> index = (index + one) % length of parking_lot
+			if index == start:
+				print "car not found"
+				return
+		print "Car not found" -> reached an empty slot, car not there
+	
 
 	define printLot(parking_lot):
 		Print "Parking lot state"
@@ -52,6 +71,7 @@ func hash(number_plate string, length int) int {
 
 }
 
+
 func insert(number_plate string, parking_lot []string) {
 	// Prevent duplicates
 	if lookup(number_plate, parking_lot) {
@@ -62,7 +82,7 @@ func insert(number_plate string, parking_lot []string) {
 	index := hash(number_plate, len(parking_lot))
 	start := index
 
-	for parking_lot[index] != "" {
+	for parking_lot[index] != "" && parking_lot[index] != "DELETED" {
 		index = (index + 1) % len(parking_lot)
 
 		if index == start {
@@ -71,7 +91,7 @@ func insert(number_plate string, parking_lot []string) {
 		}
 	}
 	parking_lot[index] = number_plate
-	fmt.Println("Car parked successfully")
+	fmt.Println("Car parked successfully", number_plate)
 }
 
 
@@ -79,21 +99,43 @@ func lookup(plate string, parking_lot []string) bool{
 	index := hash(plate, len(parking_lot))
 	start := index
 
-	for {
-		// empty slot - we would have stopped while inserting, so not found
-		if parking_lot[index] == "" {
-			return false
-		}
+	for parking_lot[index] != "" {
 		if parking_lot[index] == plate {
 			return true
 		}
+
 		index = (index + 1) % len(parking_lot)
 		if index == start {
-			// we've looped all the way around; not found
-			return false
+			break
 		}
 	}
+	return false
 }
+
+
+
+func deleteCar(number_plate string, parking_lot []string) {
+	index := hash(number_plate, len(parking_lot))
+	start := index
+
+	for parking_lot[index] != "" {
+		if parking_lot[index] == number_plate {
+			parking_lot[index] = "DELETED"
+			fmt.Println("Car removed successfully")
+			return
+		}
+		
+		index = (index + 1) % len(parking_lot)
+		if index == start {
+			fmt.Println("Car not found", number_plate)
+			return
+		}
+
+	}
+	fmt.Println("Car not found", number_plate)
+}
+
+
 
 func printLot(parking_lot []string) {
 	fmt.Println("Parking lost state")
@@ -116,7 +158,8 @@ func main() {
 		fmt.Println("1) Park car")
 		fmt.Println("2) Find car")
 		fmt.Println("3) Show parking lot (debug)")
-		fmt.Println("4) Exit")
+		fmt.Println("4) Remove a car from parking lot")
+		fmt.Println("5) Bye")
 
 		_, err := fmt.Scanln(&choice)
 
@@ -149,9 +192,15 @@ func main() {
 			printLot(parking_lot)
 
 		case 4:
+			fmt.Println("Enter the number plate to delete:")
+			var plate string
+			fmt.Scanln(&plate)
+			deleteCar(plate, parking_lot)
+
+		case 5:
 			fmt.Println("Bye")
 			return
-		
+
 		default:
 			fmt.Println("Invalid choice, pick 1-4")
 	}
